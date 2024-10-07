7c0c9afdbfd6c23337e5bc343612a2d0c0a1793e
Mukul <mukulbhardwaj966@gmail.com>
2024-10-07 12:25:49 +0530

    fix mobile layout inside banner

--- app/template.tsx
+++ app/template.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import PageWrapper from "@/wrapper/page-wrapper/PageWrapper";
 export default function template({ children }: { children: ReactNode }) {
+
     return (
         <PageWrapper>
             {children}
