d290f2be0b0af278407eed3b70ee66f1255aa452
Mukul <mukulbhardwaj966@gmail.com>
2024-10-09 15:38:38 +0530

    added correct site map and robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://bistatechnologiesdev.vercel.app/',
+    siteUrl: 'https://bistatechnologies.vercel.app/',
     exclude: ['/icon.svg', '/apple-icon.png', '/manifest.webmanifest', '/tags/*'],
     generateRobotsTxt: true,
     generateIndexSitemap: false,
